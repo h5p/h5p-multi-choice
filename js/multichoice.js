dcdcc4005ee8f23ed792c1622e1f74d62af37bb6
Frank Ronny Larsen <frank.larsen@amendor.no>
2013-01-13 09:40:55 +0100

    Several changes:
    Added h5pQuestionAnswered event trigger
    Use H5P.jQuery
    Support singlePoint result on multiAnswer quizzes
    Add totalScore() function.
    Change schema to use label instead of name.

--- js/multichoice.js
+++ js/multichoice.js
@@ -6,12 +6,29 @@
 //   question: "Question text",
 //   answers: [{text: "Answer text", correct: false}, ...],
 //   singleAnswer: true, // or false, will change rendered output slightly.
+//   singlePoint: true,  // True if question give a single point score only
+//                       // if all are correct, false to give 1 point per
+//                       // correct answer. (Only for singleAnswer=false)
+//   randomOrder: false  // Whether to randomize the order of answers.
 // }
+//
+// Events provided:
+// - h5pQuestionAnswered: Triggered when a question has been answered.
+
+// TODO list:
+//
+// Keep state of question between renderings. Show current state if user has
+// answered before!
+//
+// Actually randomize order if randomOrder=true
+
 window.H5P = window.H5P || {};
 
 H5P.MultiChoice = function (options) {
   if ( !(this instanceof H5P.MultiChoice) )
     return new H5P.MultiChoice(options);
+
+  var $ = H5P.jQuery;
 
   var texttemplate = '' +
 '<div class="multichoice">' +
@@ -34,16 +51,19 @@ H5P.MultiChoice = function (options) {
 '</div>' +
   '';
 
+  var that = this;
   var defaults = {
     title: "",
     question: "No question text provided",
     answers: [{text: "Answer 1", correct: false },
               {text: "Answer 2", correct: true }],
     singleAnswer: false,
-    randomOrder: false
+    singlePoint: true,
+    randomOrder: false,
+    weight: 1
   };
   var template = new EJS({text: texttemplate});
-  var params = jQuery.extend({}, defaults, options);
+  var params = $.extend({}, defaults, options);
   var myDom;
 
   var answerGiven = false;
@@ -58,7 +78,7 @@ H5P.MultiChoice = function (options) {
   var attach = function (targetId) {
     // Render own DOM into target.
     template.update(targetId, params);
-    myDom = jQuery('#'+targetId);
+    myDom = $('#'+targetId);
 
     // Set event listeners.
     $('input', myDom).click(function () {
@@ -74,23 +94,40 @@ H5P.MultiChoice = function (options) {
         score = 0;
         $('input', myDom).each(function (idx, el) {
           var $el = $(el);
-          // var num = parseInt($el.val().split('_')[1], 10);
           if ($el.is(':checked') == params.answers[idx].correct) {
             score += 1; // TODO: Weight of answers?
           }
         });
+        if (params.singlePoint) {
+          score = (score == params.answers.length) ? 1 : 0;
+        }
       }
+      // Triggers must be done on the returnObject.
+      $(returnObject).trigger('h5pQuestionAnswered');
     });
 
     return this;
   };
 
-  // Store options.
-  return {
+  // Masquerade the main object to hide inner properties and functions.
+  var returnObject = {
     machineName: 'H5P.MultiChoice',
     attach: attach, // Attach to DOM object
     getScore: function () {return score;},
     getAnswerGiven: function () {return answerGiven;},
+    totalScore: function () {
+      if (!params.singleAnswer && !params.singlePoint) {
+        var s = 0;
+        for (var i = params.answers.length - 1; i >= 0; i--) {
+          var a = params.answers[i];
+          s += (a.weight !== undefined) ? a.weight : 1;
+        }
+        return s;
+      }
+      return params.weight;
+    },
     defaults: defaults // Provide defaults for inspection
   };
+  // Store options.
+  return returnObject;
 };
