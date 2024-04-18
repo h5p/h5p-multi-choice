abc71c2a97ee55b007e593036b2e1a9d4fc44d04
Vilde Voss Stabell <Vilde.voss.stabell@gmail.com>
2024-04-18 13:16:55 +0200

    JI-6143 Avoid stripping html from xAPI description (#170)
    * JI-6143 Avoid stripping tags in xAPI description
    
    * JI-6143 bump
    
    ---------
    
    Co-authored-by: devland <adevland@gmail.com>

--- js/multichoice.js
+++ js/multichoice.js
@@ -913,8 +913,7 @@ H5P.MultiChoice = function (options, contentId, contentData) {
   var addQuestionToXAPI = function (xAPIEvent) {
     var definition = xAPIEvent.getVerifiedStatementValue(['object', 'definition']);
     definition.description = {
-      // Remove tags, must wrap in div tag because jQuery 1.9 will crash if the string isn't wrapped in a tag.
-      'en-US': $('<div>' + params.question + '</div>').text()
+      'en-US': params.question,
     };
     definition.type = 'http://adlnet.gov/expapi/activities/cmi.interaction';
     definition.interactionType = 'choice';
